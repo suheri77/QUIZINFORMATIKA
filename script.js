3ba8ca7f288c662f3515ac09826ed9874f532c1f
suheri77 <153284084+suheri77@users.noreply.github.com>
2024-05-14 18:50:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -255,24 +255,23 @@ function authenticate() {
     }
 }
 
-function unlockWebsite() {
-    // Tambahkan kode untuk menghilangkan penguncian situs web
-    // Contoh:
-    document.body.classList.remove("locked");
-}
-
-// Panggil fungsi autentikasi saat dokumen dimuat
-document.addEventListener("DOMContentLoaded", function() {
-    authenticate();
-});
-
-document.addEventListener("visibilitychange", function () {
-    if (document.hidden) {
-        alert("Screenshot tidak diizinkan!");
-        // Anda bisa menyembunyikan konten di sini jika diperlukan
+/** TO DISABLE SCREEN CAPTURE **/
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'PrintScreen') {
+        navigator.clipboard.writeText('');
+        alert('Screenshots disabled!');
     }
 });
 
+/** TO DISABLE PRINTS WHIT CTRL+P **/
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key == 'p') {
+        alert('This section is not allowed to print or export to PDF');
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+});
 
 
 
